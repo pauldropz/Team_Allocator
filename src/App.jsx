d47ffb64b6275025d2e13c1032785e61f91856b7
pauldropz <pauldropz@gmail.com>
2022-09-16 16:25:09 +0100

    svg removal and app.jsx importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import Content from "./Content"
+import Footer from "./Footer"
+
+
 
 function App() {
   return (
