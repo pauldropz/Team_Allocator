f3475edfd60f70302cd4f3360743692ec4f63736
pauldropz <pauldropz@gmail.com>
2022-09-17 20:41:41 +0100

    more update

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -11,7 +11,23 @@ const Employees = () => {
                 { id:3, fullName: "Suleiman Shehu", designation: "Animation", gender: "Male", teamName: "Team7"},
                 { id:4, fullName: "Mitsuki Orochimaro", designation: "Data Scientist", gender: "Male", teamName: "Team5"},
                 { id:5, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
-
+                { id:6, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:7, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:8, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:9, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:10, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:11, fullName: "Paul Dropz", designation: "Web-Developer", gender: "male", teamName: "Team7"},
+                { id:12, fullName: "Yelton Athanasius", designation: "Data Scientist", gender: "Male", teamName: "Team5"},
+                { id:13, fullName: "Suleiman Shehu", designation: "Animation", gender: "Male", teamName: "Team7"},
+                { id:14, fullName: "Mitsuki Orochimaro", designation: "Data Scientist", gender: "Male", teamName: "Team5"},
+                { id:15, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:16, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:17, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:18, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:19, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                { id:20, fullName: "White Sage", designation: "generator mofo", gender: "Male", teamName: "Team7"},
+                
+                
 
                                     ]);
 
