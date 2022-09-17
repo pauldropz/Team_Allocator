7dda9af3d2a6de87a30df5bd8ad27059b878a9fa
pauldropz <pauldropz@gmail.com>
2022-09-17 18:04:51 +0100

    2nd id making

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,7 +4,11 @@ import { useState } from "react";
 
 
 const Employee = () => {
-            const [ employees, setEmployees ] = useState ();
+            const [ employees, setEmployees ] = useState ([
+
+                { id:1, fullName: "Paul Dropz", designation: "Web-Developer", gender: "male", teamName: "TeamA"},
+                { id:2, fullName: "Yelton Athanasius", designation: "Data Scientist", gender: "Male", teamName: "TeamC"}
+                                    ]);
 
     return (
 
